5ddc084b94c12950eeb409b3f048099913124702
shinokada <okada.shin@gmail.com>
2014-04-28 18:55:48 +0900

    Complete vote.rb

--- 2014/2014-yo/lib/vote.rb
+++ 2014/2014-yo/lib/vote.rb
@@ -9,15 +9,23 @@ class Vote
     costs = file[0..sports_num - 1].map{ |n| n.to_i }  # return [8, 8, 7, 5, 2, 5, 2, 7, 2, 1]
     popu_cost = Hash[(1..sports_num).zip costs]   # return {1=>8, 2=>8, 3=>7, 4=>5, 5=>2, 6=>5, 7=>2, 8=>7, 9=>2, 10=>1}
     votes = file[sports_num..-1].map{ |e| e.to_i } # return [2, 8, 4, 1, 8, 2, 10, 3, 2, 2]
-    votes.each do
-      |l| popu_cost.each do
-        |key, val|
-        if l <= val 
-          results << key
-        end
-      end
+    votes.each do |e|
+      key, _ = popu_cost.find { |_, val| e >= val}
+          results << key if key
     end
-      results
+    most_common_value(results)
+   # results = votes.map do |vote|
+   #   popu_cost.find(-> {[]}) { |_, cost| cost <= vote }.first
+   # end
+   # results.group_by{|a| a }.sort_by{|a,b| b.size<=>a.size}.first[0]
   end
+
+
+  def most_common_value(a)
+    a.group_by do |e|
+      e
+    end.values.max_by(&:size).first
+  end
+
 
 end
--- 2014/2014-yo/spec/lib/vote_spec.rb
+++ 2014/2014-yo/spec/lib/vote_spec.rb
@@ -8,33 +8,33 @@ describe Vote do
     it { should eq 5 }
   end
 
-  # context '2014-yo-t2-in2.txt' do
-  #   let(:file_name) { File.expand_path('../../../2014-yo-data/2014-yo-t2-in2.txt', __FILE__) }
-  #   it { should eq 4 }
-  # end
+   context '2014-yo-t2-in2.txt' do
+     let(:file_name) { File.expand_path('../../../2014-yo-data/2014-yo-t2-in2.txt', __FILE__) }
+     it { should eq 4 }
+   end
 
-  # context '2014-yo-t2-in3.txt' do
-  #   let(:file_name) { File.expand_path('../../../2014-yo-data/2014-yo-t2-in3.txt', __FILE__) }
-  #   it { should eq 1 }
-  # end
+   context '2014-yo-t2-in3.txt' do
+     let(:file_name) { File.expand_path('../../../2014-yo-data/2014-yo-t2-in3.txt', __FILE__) }
+     it { should eq 1 }
+   end
 
-  # context '2014-yo-t2-in4.txt' do
-  #   let(:file_name) { File.expand_path('../../../2014-yo-data/2014-yo-t2-in4.txt', __FILE__) }
-  #   it { should eq 452 }
-  # end
+   context '2014-yo-t2-in4.txt' do
+     let(:file_name) { File.expand_path('../../../2014-yo-data/2014-yo-t2-in4.txt', __FILE__) }
+     it { should eq 452 }
+   end
   
-  # context '2014-yo-t2-in5.txt' do
-  #   let(:file_name) { File.expand_path('../../../2014-yo-data/2014-yo-t2-in5.txt', __FILE__) }
-  #   it { should eq 953 }
-  # end
+   context '2014-yo-t2-in5.txt' do
+     let(:file_name) { File.expand_path('../../../2014-yo-data/2014-yo-t2-in5.txt', __FILE__) }
+     it { should eq 953 }
+   end
 
-  # context '2014-yo-t2-in_s1.txt' do
-  #   let(:file_name) { File.expand_path('../../../2014-yo-data/2014-yo-t2-in_s1.txt', __FILE__) }
-  #   it { should eq 2 }
-  # end
+   context '2014-yo-t2-in_s1.txt' do
+     let(:file_name) { File.expand_path('../../../2014-yo-data/2014-yo-t2-in_s1.txt', __FILE__) }
+     it { should eq 2 }
+   end
 
-  # context '2014-yo-t2-in_s2.txt' do
-  #   let(:file_name) { File.expand_path('../../../2014-yo-data/2014-yo-t2-in_s2.txt', __FILE__) }
-  #   it { should eq 1 }
-  # end
+   context '2014-yo-t2-in_s2.txt' do
+     let(:file_name) { File.expand_path('../../../2014-yo-data/2014-yo-t2-in_s2.txt', __FILE__) }
+     it { should eq 1 }
+   end
 end
