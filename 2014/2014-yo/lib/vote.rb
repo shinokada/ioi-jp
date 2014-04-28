a09bf5d2d20dacbc037d70ce1ba764864100ece7
shinokada <okada.shin@gmail.com>
2014-04-28 19:09:36 +0900

    Add note to vote.rb

--- 2014/2014-yo/lib/vote.rb
+++ 2014/2014-yo/lib/vote.rb
@@ -11,21 +11,29 @@ class Vote
     votes = file[sports_num..-1].map{ |e| e.to_i } # return [2, 8, 4, 1, 8, 2, 10, 3, 2, 2]
     votes.each do |e|
       key, _ = popu_cost.find { |_, val| e >= val}
-          results << key if key
+          results << key if key # results returns [5, 1, 5, 10, 1, 5, 1, 5, 5, 5]
     end
     most_common_value(results)
-   # results = votes.map do |vote|
+   # results = votes.map do |vote| # same as above
    #   popu_cost.find(-> {[]}) { |_, cost| cost <= vote }.first
    # end
    # results.group_by{|a| a }.sort_by{|a,b| b.size<=>a.size}.first[0]
   end
-
 
   def most_common_value(a)
     a.group_by do |e|
       e
     end.values.max_by(&:size).first
   end
+  # notes on most_common_value
+  # upto end returns
+  # {5=>[5, 5, 5, 5, 5, 5], 1=>[1, 1, 1], 10=>[10]}
+  # upto .values returns
+  # [[5, 5, 5, 5, 5, 5], [1, 1, 1], [10]]
+  # 
+  # upto values.max_by(&:size) returns
+  # [5, 5, 5, 5, 5, 5]
+
 
 
 end
