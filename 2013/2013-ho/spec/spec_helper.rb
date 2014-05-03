61cfe318c0f6ca2dc2628ef45290ce5503cf190e
shinokada <okada.shin@gmail.com>
2014-05-03 20:46:08 +0900

    Added 2013-ho

---
+++ 2013/2013-ho/spec/spec_helper.rb
@@ -0,0 +1,28 @@
+$: << File.join(File.dirname(__FILE__), '..', 'lib')
+
+require 'pry' if ENV['APP_ENV'] == 'debug' # add `binding.pry` wherever you need to debug
+require 'illumination'
+require 'ioitrain'
+require 'modernmansion'
+require 'towerofjoioi'
+require 'bubblesort'
+
+def read_file_to_arr(file_name)
+  # ['17, 10, 16 3, 7']
+  # ["4", "bar", "abracadabra", "bear", "bar", "baraxbara"] 
+  File.readlines(file_name).map(&:chomp)
+end
+
+def read_file_to_str(file_name)
+  # '17, 10, 16, 3, 7'
+  # "4 bar abracadabra bear bar baraxbara"
+  File.readlines(file_name).map(&:chomp).join(' ')
+end
+
+def read_num_to_arr(file_name)
+  # [17, 10, 16, 3, 7]
+  File.readlines(file_name).map(&:chomp).map(&:to_i)
+end
+
+
+
