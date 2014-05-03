22b7684100abdfbe518f03d2ac353ace4aa53e85
shinokada <okada.shin@gmail.com>
2014-05-03 20:44:45 +0900

    Start 2013-ho

--- 2013/2013-yo/lib/signboard.rb
+++ 2013/2013-yo/lib/signboard.rb
@@ -1,8 +1,22 @@
 class Signboard
 
   def self.num_signboard(file_name)
-    file = File.readlines(file_name).map(&:chomp).join(' ')
-    #file.shift
+    file = File.readlines(file_name).map(&:chomp)
+    input_num, shop_name, *boards = file.join(',').split(',')
+
+  #  re = shop_name.gsub(//, '[a-z]' )
+#re = /[a-z]*b[a-z]a[a-z]r/
+    #re = /[a-z]*b(?<bal>a|.\g<bal>.)r/
+ 
+    count = 0
+    boards.each do |e| 
+      if e =~ re
+        count += 1 
+      else
+        count = count 
+      end
+    end
+    count 
   end
 
 end
--- 2013/2013-yo/spec/lib/signboard_spec.rb
+++ 2013/2013-yo/spec/lib/signboard_spec.rb
@@ -7,8 +7,30 @@ describe Signboard do
     it { should eq 3 }
   end
 
+  context '2013-yo-data/2013-yo-t3-in1' do
+    let(:file_name){ File.expand_path('../../../2013-yo-data/2013-yo-t3-in1.txt', __FILE__)  }
+    it { should eq 7 }
+  end
 
+  context '2013-yo-data/2013-yo-t3-in2' do
+    let(:file_name){ File.expand_path('../../../2013-yo-data/2013-yo-t3-in2.txt', __FILE__)  }
+    it { should eq 11 }
+  end
 
+  context '2013-yo-data/2013-yo-t3-in3' do
+    let(:file_name){ File.expand_path('../../../2013-yo-data/2013-yo-t3-in3.txt', __FILE__)  }
+    it { should eq 63 }
+  end
+ 
+  context '2013-yo-data/2013-yo-t3-in4' do
+    let(:file_name){ File.expand_path('../../../2013-yo-data/2013-yo-t3-in4.txt', __FILE__)  }
+    it { should eq 42 }
+  end
+  
+  context '2013-yo-data/2013-yo-t3-in5' do
+    let(:file_name){ File.expand_path('../../../2013-yo-data/2013-yo-t3-in5.txt', __FILE__)  }
+    it { should eq 47 }
+  end
 
 end
 
